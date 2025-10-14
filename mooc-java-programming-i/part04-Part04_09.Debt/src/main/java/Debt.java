1dbea5ed9aae94dbdde07f6ff1c3dc499e12c0e7
kataali <muhammadismaaiil360@gmail.com>
2025-10-14 17:53:42 +0000

    Debt exercise done

---
+++ mooc-java-programming-i/part04-Part04_09.Debt/src/main/java/Debt.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kataali
+ */
+public class Debt {
+    private double balance;
+    private double interestRate;
+//    Constructor
+    public Debt(double initialBalance, double initialInterestRate){
+        this.balance = initialBalance;
+        this.interestRate = initialInterestRate;
+    }
+//    Method
+    public void printBalance(){
+        System.out.println(this.balance);
+    } 
+//    Method 
+    public void waitOneYear(){
+        this.balance = this.balance * this.interestRate;
+    }
+}
