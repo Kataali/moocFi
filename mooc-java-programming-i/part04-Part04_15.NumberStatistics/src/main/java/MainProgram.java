35c2f1ebb35f1e89b1f28e1f1552f7bbb57cfe4f
kataali <muhammadismaaiil360@gmail.com>
2025-10-17 16:24:41 +0000

    NumberStatistics exercise done

--- mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,12 +5,31 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
-
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
+        Statistics stat = new Statistics();
+        Statistics statsEven = new Statistics();
+        Statistics statsOdd = new Statistics();
+        System.out.println("Enter numbers:");
+        while (true) {
+         try {
+        int number = Integer.parseInt(scanner.nextLine());
+        if (number != -1){
+            stat.addNumber(number);
+            System.out.println(stat.average());
+            if (number % 2 == 0){
+                statsEven.addNumber(number);
+            }else{
+                statsOdd.addNumber(number);
+            }
+        } else {
+            break;
+        }
+         } catch (Exception e){
+         continue;
+        }
+        }
+        System.out.println("Sum: " + stat.sum());
+        System.out.println("Sum of even numbers: " + statsEven.sum());
+        System.out.println("Sum of odd numbers: " + statsOdd.sum());
+        
     }
 }
--- mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,36 @@
 
 public class Statistics {
+    private int count;
+    private int sum;
+//    Constructor
+    public Statistics(){
+        this.count = 0;
+        this.sum = 0;
+    }
+//    Method
+    public void addNumber(int number) {
+        this.count++;
+        this.sum += number;
+    }
+//    Method
+    public int getCount() {
+        return this.count;
+    }
+//    Method
+    public int sum() {
+        return this.sum;
+    }
+//Method
+    public double average() {
+        try {
+            if (this.count == 0){
+                return 0;
+            } else {
+            return this.sum * 1.0 / this.count;
+            }
+        } catch (Exception e) {
+         return 0;
+        }
+        
+   }
 }
