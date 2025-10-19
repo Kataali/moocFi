466dac2d8fcd3f13e53dc3d065d6184a22935351
kataali <muhammadismaaiil360@gmail.com>
2025-10-19 21:18:48 +0000

    TelevisionProgram exercise done

--- mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -8,6 +8,26 @@ public class Main {
 
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        
+        while(true){
+            System.out.println("Name: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()){
+                break;
+            }
+            System.out.println("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+            
+            TelevisionProgram program = new TelevisionProgram(name, duration);
+            programs.add(program);
+        }   
+        
+        System.out.println("Program's maximum duration? ");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+        for (TelevisionProgram program : programs){
+            if (program.getDuration() <= maxDuration){
+                System.out.println(program.getName() + ", " + program.getDuration());
+            }
+        }
     }
 }
