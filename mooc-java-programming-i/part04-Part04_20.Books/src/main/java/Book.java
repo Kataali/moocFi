610842ecc569fdad59287557ae5dd28d6e18035a
kataali <muhammadismaaiil360@gmail.com>
2025-10-19 21:49:15 +0000

    Books exercise done

---
+++ mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Book.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kataali
+ */
+public class Book {
+   private String title;
+   private int pages;
+   private int publicationYear;
+   
+   public Book(String title, int pages, int publicationYear){
+       this.title = title;
+       this.pages = pages;
+       this.publicationYear = publicationYear;
+   }
+   
+   public String toString(){
+       return this.title + ", " + this.pages + " pages, " + this.publicationYear;
+   }
+   
+   public String getName(){
+       return this.title;
+   }
+}
--- mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
+++ mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,32 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
-
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList();
+        
+        while(true){
+            System.out.println("Title: ");
+            String title = scanner.nextLine();
+            if (title.isEmpty()){
+                break;
+            }
+            System.out.println("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+            System.out.println("Publication year: ");
+            int pubYear = Integer.valueOf(scanner.nextLine());
+            Book curBook = new Book(title, pages, pubYear);
+            books.add(curBook);
+            }
+        
+            System.out.println("What information would you like to be printed?");
+            String answer = scanner.nextLine();
+            boolean isEverything = answer.toLowerCase().equals("everything");
+            for (Book book : books){
+                if (isEverything){
+                    System.out.println(book);
+                } else {
+                    System.out.println(book.getName());
+                }
+        }
     }
 }
