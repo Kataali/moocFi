8eca734e81c85c8a27da0680e7cf5b337fbdac4f
kataali <muhammadismaaiil360@gmail.com>
2025-10-19 21:03:34 +0000

    PersonalInformation exercise done

--- mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -9,6 +9,27 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.print("First name: ");
+            String firstName = scanner.nextLine();
+            if (firstName.isEmpty()) {
+                break;
+            }
 
+            System.out.print("Last name: ");
+
+            String lastName = scanner.nextLine();
+
+            System.out.println("Identification number: ");
+            String id = scanner.nextLine();
+
+            // We add a new person to the list.
+            // The person's name and age were decided by the user
+            infoCollection.add(new PersonalInformation(firstName, lastName, id));
+        }
+        System.out.println("\n");
+        for (PersonalInformation info : infoCollection){
+            System.out.println(info.getFirstName().concat(" " + info.getLastName()));
+        }
     }
 }
