f9297c796993bc25b623a8d1188079294cc5e7fb
kataali <muhammadismaaiil360@gmail.com>
2025-10-21 09:01:44 +0000

    PrintingAFile exercise done

--- mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class PrintingAFile {
 
     public static void main(String[] args) {
-
+        try {
+            Scanner textFile = new Scanner(Paths.get("data.txt"));
+            while (textFile.hasNextLine()){
+                String row = textFile.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
