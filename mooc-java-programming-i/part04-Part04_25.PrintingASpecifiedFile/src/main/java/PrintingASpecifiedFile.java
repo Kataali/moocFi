09c2507411be62f24098b51c2c0aa3fbc5079983
kataali <muhammadismaaiil360@gmail.com>
2025-10-23 15:09:49 +0000

    PrintingASpecificFile exercise done

--- mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,16 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Which file should have its contents printed?");
+        String fileName = scanner.nextLine();
+        try {
+            Scanner textFile = new Scanner(Paths.get(fileName));
+            while (textFile.hasNextLine()){
+                String row = textFile.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
