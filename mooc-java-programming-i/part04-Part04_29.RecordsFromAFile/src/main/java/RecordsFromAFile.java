118b7eeae503ab2b15ef98a7f702ff7fa847bb8d
kataali <muhammadismaaiil360@gmail.com>
2025-10-23 21:29:09 +0000

    RecordsFromAFile exercise done

--- mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,21 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Name of the file: ");
+        String file = scanner.nextLine();
+        try {
+            Scanner textFile = new Scanner(Paths.get(file));
+            boolean found = false;
+            while (textFile.hasNextLine()){
+                String row = textFile.nextLine();
+                String[] parts = row.split(",");
+                String name = parts[0];
+                String age = parts[1];
+                String qualifier = (Integer.valueOf(age) == 1)? " year" : " years";
+                System.out.println(name + ", age: " + age + qualifier );
+            }
+        } catch (Exception e){
+            System.out.println("Reading the file " + file + " failed.");
+        }
     }
 }
